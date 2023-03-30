c42849c332fca2acf88aff1b9e339c6997581bfa
Tobias Batch <tobias@neontribe.co.uk>
2023-03-30 10:01:20 +0100

    docs: the settings module

--- src/main/java/uk/co/neontribe/kimai/Main.java
+++ src/main/java/uk/co/neontribe/kimai/Main.java
@@ -1,13 +1,30 @@
 package uk.co.neontribe.kimai;
 
+import uk.co.neontribe.kimai.config.ConfigNotInitialisedException;
 import uk.co.neontribe.kimai.config.Settings;
 import uk.co.neontribe.kimai.desktop.KimaiUiFrame;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    /**
+     * The entry point. This will start our application.
+     *
+     * @param args String[]
+     * @throws ConfigNotInitialisedException Thrown when config location cannot be created.
+     * @throws IOException Thrown if we can't read from the file system
+     */
+    public static void main(String[] args) throws ConfigNotInitialisedException, IOException {
+        // Create a new application frame and show it.
+        KimaiUiFrame frame = new KimaiUiFrame();
+        frame.setVisible(true);
 
-        KimaiUiFrame kuif = new KimaiUiFrame();
+        // Read settings from the file system
+        Settings settings = Settings.getInstance();
 
-        kuif.setVisible(true);
+        // Print the values to the file system.
+        System.out.println(settings.getKimaiUri());
+        System.out.println(settings.getKimaiUsername());
+        System.out.println(settings.getKimaiPassword());
     }
 }
