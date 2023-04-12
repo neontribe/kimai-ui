51c385e5ac6c76b88c13659170b89992d2a19437
Tobias Batch <tobias@neontribe.co.uk>
2023-04-12 16:02:46 +0100

    chore: jet brains code advice

--- src/main/java/uk/co/neontribe/kimai/desktop/KimaiUiFrame.java
+++ src/main/java/uk/co/neontribe/kimai/desktop/KimaiUiFrame.java
@@ -9,21 +9,19 @@ import java.io.IOException;
 
 public class KimaiUiFrame extends JFrame {
 
-    private Settings settings;
-
     public KimaiUiFrame() throws IOException {
         super("Kimai Desktop UI");
 
         // Read settings from the file system
-        settings = null;
+        Settings settings = null;
         try {
             settings = Settings.getInstance();
         } catch (IOException e) {
             throw new RuntimeException("Cannot continue, home space is not writable", e);
         } catch (ConfigNotInitialisedException e) {
             System.err.println("Config is not initialised.");
-            this.settings = new Settings();
-            ConfigPanel.makeFrame(this, this.settings).setVisible(true);
+            settings = new Settings();
+            ConfigPanel.makeFrame(this, settings).setVisible(true);
         }
 
         this.add(new TimeEntryPanel());
